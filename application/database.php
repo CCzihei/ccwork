9054b16414776e1d8f21766de1affe482f79de93
czh <1575810034@qq.com>
2018-02-02 03:06:43 +0800

    数据库修改

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '118.31.13.184',
     // 数据库名
     'database'        => 'ch_chat',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '123456',
+    'password'        => '57Ebf8948c87',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
